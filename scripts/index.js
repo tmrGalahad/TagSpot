ac54dddd27d4ada17de33b7499079cda22b1e471
tmrGalahad <gxlahadk@gmail.com>
2026-04-25 15:27:54 +0500

    add card creation and deletion

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-
+function createCard(cardData, handleDelete) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+    deleteButton.addEventListener('click', handleDelete);
+    return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(evt) {
+    const card = evt.target.closest('.card');
+    card.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(cardData => {
+    const card = createCard(cardData, deleteCard);
+    placesList.append(card);
+});
